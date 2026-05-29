7e63556fabd4918d0a8d883f599edb75f88d155c
Ben Campbell <hirebencampbell@gmail.com>
2026-05-29 18:10:08 -0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import VisceralFatGuide from './components/VisceralFatGuide';
+
+export default function App() {
+  return (
+    // This utilizes your steady-app-shell background gradients perfectly!
+    <div className="steady-app-shell min-h-screen w-full">
+      <VisceralFatGuide />
+    </div>
+  );
+}
